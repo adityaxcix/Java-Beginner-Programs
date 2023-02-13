99b25df86c1a20db6fda7b1760634664161ff717
Aditya Gupta <117362157+adityaxcix@users.noreply.github.com>
2023-02-13 22:33:07 +0530

    Add files via upload

---
+++ sumOfElementsOfArray.java
@@ -0,0 +1,16 @@
+
+public class sumOfElementsOfArray 
+{
+
+	public static void main(String[] args) 
+	{
+		int arr[] = {5,5,5,10};
+		int sum=0;
+		for(int i =0;i<arr.length;i++)
+		{
+			sum=sum+arr[i];
+		}
+System.out.println(sum);
+	}
+
+}
