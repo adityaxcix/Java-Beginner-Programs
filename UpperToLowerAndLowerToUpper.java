3f018146e5034c8c0549694f1dea8ffd6c9f287a
Aditya Gupta <117362157+adityaxcix@users.noreply.github.com>
2023-02-28 18:58:10 +0530

    Add files via upload

---
+++ UpperToLowerAndLowerToUpper.java
@@ -0,0 +1,21 @@
+package stringPrograms1_10;
+public class UpperToLowerAndLowerToUpper
+{
+	public static void main(String[] args) 
+	{
+		//Converting UpperCase to LowerCase ? Don't worry, Just add 32 and Vice versa
+		
+		//UpperCase
+			char ch = 'A';
+			ch+=32;
+			System.out.println(ch);
+		//LowerCase
+			char ch1 = 'a';
+			ch1-=32;
+			System.out.println(ch1);
+			
+			
+			
+			
+	}
+}
